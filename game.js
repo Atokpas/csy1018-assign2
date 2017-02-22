7ebebafb4e17c6ca402e38eaa1f25aa2137165a2
Alfie England <Alfie England>
2017-02-22 22:28:16 +0000

    not sure what I did, but i did something

--- game.js
+++ game.js
@@ -1,31 +1,20 @@
 var interval = 0;
 
 function startRace() {
-	var character = document.getElementById('character');
-	var positionTop = character.offsetTop;
+	var raceTimer = setInterval(100);
+	var raceActive = true;
 	console.log('Starting Race!');
-	/*var positionLeft = character.offsetLeft;*/
-	character.style.top = positionTop + 1 + 'px';
+	horse1();
+}
+
+function horse1() {
+	var horse1 = document.getElementById('horse1');
+	var positionTop = horse1.offestTop;
+	var positionLeft = horse1.offsetLeft;
+	horse1.style.left = positionLeft + 1 + 'px';
 }
 
 function myLoadFunction() {
-	/*player = document.getElementById('player');
-
-	var heads = document.getElementsByClassName('heads')[0].getElementsByTagName('li');
-	for (var i = 0; i < heads.length; i++) {
-		heads[i].addEventListener('click', setHead);
-	}
-
-	var bodies = document.getElementsByClassName('bodies')[0].getElementsByTagName('li');
-
-	for (var i = 0; i < heads.length; i++) {
-		bodies[i].addEventListener('click', setBody);
-	}
-
-	timeout = setInterval(move, 10);
-	document.addEventListener('keydown', keydown);
-	document.addEventListener('keyup', keyup);*/
-
 	var startButton = document.getElementById('start');
 	startButton.addEventListener('click', startRace);
 }
