a49e0b1d45ea3082ba1c3acd34611775647d4986
Alfie England <Alfie England>
2017-02-23 19:13:55 +0000

    All horses now move on race start

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ function startRace() {
 	loop();
 }
 
-function loop() {
+/*function loop() {
 	interval = setInterval(horse1, 10);
 }
 
@@ -15,10 +15,27 @@ function horse1() {
 	var horse1 = document.getElementById('horse1');
 	var positionTop = horse1.offestTop;
 	var positionLeft = horse1.offsetLeft;
-	if (positionLeft >= window.innerWidth * 0.85 - 50){
+	if (positionLeft >= window.innerWidth * 0.85){
 
 	} else {
 		horse1.style.left = positionLeft + 1 + 'px';
+	}
+}*/
+
+function loop() {
+	interval = setInterval(moveHorse, 10);
+}
+
+function moveHorse() {
+	var horse = document.getElementsByClassName('horse');
+	var positionTop = horse1.offestTop;
+	var positionLeft = horse1.offsetLeft;
+	for (var num = 0; num < horse.length; num++) {
+		if (positionLeft >= window.innerWidth * 0.85 - 50){
+
+		} else {
+			horse[num].style.left = positionLeft + 1 + 'px';
+		}
 	}
 }
 
