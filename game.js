3c583f821faf7e98b9f2a6f9d9b93ebe026b71f4
Alfie England <Alfie England>
2017-02-24 01:35:27 +0000

    Re-wrote if statements to be more clear

--- game.js
+++ game.js
@@ -24,18 +24,29 @@ function horse1() {
 
 // Non-repeated horse moving (all four horses), however speeds are not randomised
 function loop() {
-	interval = setInterval(moveHorse, 10);
+	interval = setInterval(moveHorse, 2);
 }
 
 function moveHorse() {
 	var horse = document.getElementsByClassName('horse');
-	var positionTop = horse1.offestTop;
+	var positionTop = horse1.offsetTop;
 	var positionLeft = horse1.offsetLeft;
 	for (var num = 0; num < horse.length; num++) {
-		if (positionLeft >= window.innerWidth * 0.85 - 50 - (num*window.innerWidth*0.02)){
-
-		} else {
+		if (positionLeft >= window.innerWidth * 0.2 && positionLeft <= window.innerWidth * 0.85 - 50 - (num*window.innerWidth*0.02) && positionTop <= window.innerHeight * 0.25 - 50 - (num*window.innerHeight * 0.04) && positionTop >= window.innerHeight * 0.05 - 50 - (num*window.innerHeight * 0.04)) {
 			horse[num].style.left = positionLeft + 1 + 'px';
+			console.log("MoveRight");
+		}
+		if (positionLeft >= window.innerWidth * 0.85 - 50 - (num*window.innerWidth*0.02) && positionLeft <= window.innerWidth * 0.95 - 50 - (num*window.innerWidth*0.02) && positionTop <= window.innerHeight * 0.85 - 50 - (num*window.innerHeight * 0.04) && positionTop >= window.innerHeight * 0.05 - 50 - (num*window.innerHeight * 0.04)) {
+			horse[num].style.top = positionTop + 1 + 'px';
+			console.log("MoveDown");
+		}
+		if (positionLeft >= window.innerWidth * 0.1 - 50 - (num*window.innerWidth*0.02) && positionLeft <= window.innerWidth * 0.95 - 50 - (num*window.innerWidth*0.02) && positionTop <= window.innerHeight * 0.98 - 50 - (num*window.innerHeight * 0.04) && positionTop >= window.innerHeight * 0.85 - 50 - (num*window.innerHeight * 0.04)) {
+			horse[num].style.left = positionLeft - 1 + 'px';
+			console.log("MoveLeft");
+		}
+		if (positionLeft >= window.innerWidth * 0.1 - 50 - (num*window.innerWidth*0.02) && positionLeft <= window.innerWidth * 0.15 - 50 - (num*window.innerWidth*0.02) && positionTop <= window.innerHeight * 0.98 - 50 - (num*window.innerHeight * 0.04) && positionTop >= window.innerHeight * 0.2 - 50 - (num*window.innerHeight * 0.04)) {
+			horse[num].style.top = positionTop - 1 + 'px';
+			console.log("MoveUp");
 		}
 	}
 }
