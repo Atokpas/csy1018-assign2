c73e20fc7adfefd7a838e12ee1af5091c31b3f53
Alfie England <Alfie England>
2017-03-10 18:32:45 +0000

    Added random number to horse movement speed during race, disabled Start button during race

--- game.js
+++ game.js
@@ -1,30 +1,14 @@
 var interval = 0;
 
 function startRace() {
-	var raceTimer = setInterval(100);
-	var raceActive = true;
 	console.log('Starting Race!');
 	loop();
 }
 
-/*function loop() {
-	interval = setInterval(horse1, 10);
-}
-
-function horse1() {
-	var horse1 = document.getElementById('horse1');
-	var positionTop = horse1.offestTop;
-	var positionLeft = horse1.offsetLeft;
-	if (positionLeft >= window.innerWidth * 0.85){
-
-	} else {
-		horse1.style.left = positionLeft + 1 + 'px';
-	}
-}*/
-
-// Non-repeated horse moving (all four horses), however speeds are not randomised
 function loop() {
-	interval = setInterval(moveHorse, 24);
+	interval = setInterval(moveHorse, 40);
+	var startButton = document.getElementById('start');
+	startButton.removeEventListener('click', startRace);
 }
 
 var raceOver = false;
@@ -36,21 +20,23 @@ function moveHorse() {
 		var positionTop = horse[num].offsetTop;
 		var positionLeft = horse[num].offsetLeft;
 		if (raceOver == false && positionLeft >= window.innerWidth * 0.15 && positionLeft <= window.innerWidth * 0.84 - 50 - (num*window.innerWidth*0.02) && positionTop <= window.innerHeight * 0.36 - 30 - (num*window.innerHeight * 0.04) && positionTop >= window.innerHeight * 0.05 - 50 - (num*window.innerHeight * 0.04)) {
-			horse[num].style.left = positionLeft + (16-num) + 'px';
+			horse[num].style.left = positionLeft + (16-num) + (Math.random() * 10) + 'px';
 			horse[num].className = 'horse runRight';
-		} else if (positionLeft >= window.innerWidth * 0.84 - 50 - (num*window.innerWidth*0.02) && positionLeft <= window.innerWidth * 0.95 - 50 - (num*window.innerWidth*0.02) && positionTop <= window.innerHeight * 0.85 - 50 - (num*window.innerHeight * 0.04) && positionTop >= window.innerHeight * 0.05 - 50 - (num*window.innerHeight * 0.04)) {
-			horse[num].style.top = positionTop + (16-num) + 'px';
+		} else if (raceOver == false && positionLeft >= window.innerWidth * 0.84 - 50 - (num*window.innerWidth*0.02) && positionLeft <= window.innerWidth * 0.95 - 50 - (num*window.innerWidth*0.02) && positionTop <= window.innerHeight * 0.85 - 50 - (num*window.innerHeight * 0.04) && positionTop >= window.innerHeight * 0.05 - 50 - (num*window.innerHeight * 0.04)) {
+			horse[num].style.top = positionTop + (16-num) + (Math.random() * 10) + 'px';
 			horse[num].className = 'horse runDown';
-		} else if (positionLeft >= window.innerWidth * 0.08 - 50 + (num*window.innerWidth*0.02) && positionLeft <= window.innerWidth * 0.95 - 50 - (num*window.innerWidth*0.02) && positionTop <= window.innerHeight * 0.98 - 50 - (num*window.innerHeight * 0.04) && positionTop >= window.innerHeight * 0.85 - 50 - (num*window.innerHeight * 0.04)) {
-			horse[num].style.left = positionLeft - (16-num) + 'px';
+		} else if (raceOver == false && positionLeft >= window.innerWidth * 0.08 - 50 + (num*window.innerWidth*0.02) && positionLeft <= window.innerWidth * 0.95 - 50 - (num*window.innerWidth*0.02) && positionTop <= window.innerHeight * 0.98 - 50 - (num*window.innerHeight * 0.04) && positionTop >= window.innerHeight * 0.85 - 50 - (num*window.innerHeight * 0.04)) {
+			horse[num].style.left = positionLeft - (16-num) + (Math.random() * 10) + 'px';
 			horse[num].className = 'horse runLeft';
 		} else if (positionLeft >= window.innerWidth * 0.05 - 50 - (num*window.innerWidth*0.02) && positionLeft <= window.innerWidth * 0.26 - (num*window.innerWidth*0.02) && positionTop <= window.innerHeight * 0.98 - 50 - (num*window.innerHeight * 0.04) && positionTop >= window.innerHeight * 0.08 - 50 + (num*window.innerHeight * 0.04)) {
-			horse[num].style.top = positionTop - (16-num) + 'px';
+			horse[num].style.top = positionTop - (16-num) + (Math.random() * 10) + 'px';
 			horse[num].className = 'horse runUp';
 		} else if (positionLeft >= window.innerWidth * 0.05 && positionLeft < window.innerWidth * 0.34 - 50 && positionTop <= window.innerHeight * 0.35 - 50 - (num*window.innerHeight * 0.04) && positionTop >= window.innerHeight * 0.05 - 50) {
 			horse[num].style.left = positionLeft + (6-num) + 'px';
 			horse[num].className = 'horse runRight';
 			raceOver = true;
+			var startButton = document.getElementById('start');
+			startButton.addEventListener('click', startRace);
 		}
 	}
 }
